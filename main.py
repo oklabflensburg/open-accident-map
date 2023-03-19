ca3db24f0fb4d3020f5e500cf655a4923a249b39
Aurelius Wendelken <webtobesocial@gmail.com>
2023-03-19 21:47:07 +0100

    first draft calling database with postgis query

---
+++ main.py
@@ -0,0 +1,60 @@
+#!./venv/bin/python
+
+from fastapi import FastAPI
+from pydantic import BaseSettings
+
+import psycopg2
+
+
+class Settings(BaseSettings):
+    app_name: str = 'Open accident map'
+    admin_email: str
+    db_host: str
+    db_user: str
+    db_name: str
+    db_port: int
+    db_pass: str
+
+    class Config:
+        env_file = '.env'
+
+app = FastAPI()
+setting = Settings()
+
+
+db_connection = psycopg2.connect(
+    host = setting.db_host,
+    port = setting.db_port,
+    dbname = setting.db_name,
+    password = setting.db_pass,
+    user = setting.db_user
+)
+
+db_connection.set_session(readonly = True)
+cur = db_connection.cursor()
+
+
+@app.get('/query/{geometry_string}')
+async def geometry_filter(geometry_string: str = None):
+    # Note: for testing purposes f-strings or string concatenation are fine but never to be used in production!
+    # Always use parametrized queries instead!
+
+    db_query = '''
+    SELECT jsonb_build_object(
+        'type', 'Feature',
+        'id', objectid,
+        'geometry', ST_AsGeoJSON(ST_Transform(a.wkb_geometry, 4326))::jsonb,
+        'properties', to_jsonb(a) - 'objectid' - 'linrefx' - 'linrefy' - \
+            'xgcswgs84' - 'ygcswgs84' - 'wkb_geometry' - 'uidentstla' - 'ogc_fid'
+    )
+    FROM postgis_unfallorte as a
+    JOIN postgis_verwaltungsgebiete as v
+    ON ST_WITHIN(a.wkb_geometry, v.wkb_geometry)
+    WHERE v.gen = %(geometry_string)s
+    AND a.ustrzustan = '1';
+    '''
+
+    # execute the query safely
+    cur.execute(db_query, {'geometry_string': geometry_string})
+
+    return cur.fetchall()
