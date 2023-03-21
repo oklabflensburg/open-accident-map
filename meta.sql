1dd115dc6a25607bfcfd56902691ad7f758e0c09
Aurelius Wendelken <webtobesocial@gmail.com>
2023-03-21 12:10:05 +0100

    updated sql meta insert query file

--- meta.sql
+++ meta.sql
@@ -1,211 +1,198 @@
-DROP TABLE IF EXISTS uland;
+DROP TABLE IF EXISTS postgis_uland;
 
-CREATE TABLE uland (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  uland NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_uland (
+  id SERIAL PRIMARY KEY,
+  uland VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS umonat;
+DROP TABLE IF EXISTS postgis_umonat;
 
-CREATE TABLE umonat (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  umonat NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_umonat (
+  id SERIAL PRIMARY KEY,
+  umonat VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS uwochentag;
+DROP TABLE IF EXISTS postgis_uwochentag;
 
-CREATE TABLE uwochentag (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  uwochentag NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_uwochentag (
+  id SERIAL PRIMARY KEY,
+  uwochentag VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS ukategorie;
+DROP TABLE IF EXISTS postgis_ukategorie;
 
-CREATE TABLE ukategorie (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  ukategorie NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_ukategorie (
+  id SERIAL PRIMARY KEY,
+  ukategorie VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS uart;
+DROP TABLE IF EXISTS postgis_uart;
 
-CREATE TABLE uart (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  uart NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_uart (
+  id SERIAL PRIMARY KEY,
+  uart VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS utyp1;
+DROP TABLE IF EXISTS postgis_utyp1;
 
-CREATE TABLE utyp1 (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  utyp1 NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_utyp1 (
+  id SERIAL PRIMARY KEY,
+  utyp1 VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS ulichtverh;
+DROP TABLE IF EXISTS postgis_ulichtverh;
 
-CREATE TABLE ulichtverh (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  ulichtverh NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_ulichtverh (
+  id SERIAL PRIMARY KEY,
+  ulichtverh VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS istrad;
+DROP TABLE IF EXISTS postgis_istrad;
 
-CREATE TABLE istrad (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  istrad NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_istrad (
+  id SERIAL PRIMARY KEY,
+  istrad VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS istpkw;
+DROP TABLE IF EXISTS postgis_istpkw;
 
-CREATE TABLE istpkw (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  istpkw NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_istpkw (
+  id SERIAL PRIMARY KEY,
+  istpkw VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS istfuss;
+DROP TABLE IF EXISTS postgis_istfuss;
 
-CREATE TABLE istfuss (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  istfuss NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_istfuss (
+  id SERIAL PRIMARY KEY,
+  istfuss VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS istkrad;
+DROP TABLE IF EXISTS postgis_istkrad;
 
-CREATE TABLE istkrad (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  istkrad NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_istkrad (
+  id SERIAL PRIMARY KEY,
+  istkrad VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS istgkfz;
+DROP TABLE IF EXISTS postgis_istgkfz;
 
-CREATE TABLE istgkfz (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  istgkfz NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_istgkfz (
+  id SERIAL PRIMARY KEY,
+  istgkfz VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS istsonstig;
+DROP TABLE IF EXISTS postgis_istsonstig;
 
-CREATE TABLE istsonstig (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  istsonstig NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_istsonstig (
+  id SERIAL PRIMARY KEY,
+  istsonstig VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS ustrzustan;
+DROP TABLE IF EXISTS postgis_ustrzustan;
 
-CREATE TABLE ustrzustan (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  ustrzustan NUMERIC NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE postgis_ustrzustan (
+  id SERIAL PRIMARY KEY,
+  ustrzustan VARCHAR(255) NOT NULL
 );
 
-INSERT INTO uland(uland, name) VALUES (01, 'Schleswig-Holstein');
-INSERT INTO uland(uland, name) VALUES (02, 'Hamburg');
-INSERT INTO uland(uland, name) VALUES (03, 'Niedersachsen');
-INSERT INTO uland(uland, name) VALUES (04, 'Bremen');
-INSERT INTO uland(uland, name) VALUES (05, 'Nordrhein-Westfalen');
-INSERT INTO uland(uland, name) VALUES (06, 'Hessen');
-INSERT INTO uland(uland, name) VALUES (07, 'Rheinland-Pfalz');
-INSERT INTO uland(uland, name) VALUES (08, 'Baden-Württemberg');
-INSERT INTO uland(uland, name) VALUES (09, 'Bayern');
-INSERT INTO uland(uland, name) VALUES (10, 'Saarland');
-INSERT INTO uland(uland, name) VALUES (11, 'Berlin');
-INSERT INTO uland(uland, name) VALUES (12, 'Brandenburg');
-INSERT INTO uland(uland, name) VALUES (13, 'Mecklenburg-Vorpommern');
-INSERT INTO uland(uland, name) VALUES (14, 'Sachsen');
-INSERT INTO uland(uland, name) VALUES (15, 'Sachsen-Anhalt');
-INSERT INTO uland(uland, name) VALUES (16, 'Thüringen');
+-- ULAND Bundesland
+INSERT INTO postgis_uland(id, uland) VALUES (1, 'Schleswig-Holstein');
+INSERT INTO postgis_uland(id, uland) VALUES (2, 'Hamburg');
+INSERT INTO postgis_uland(id, uland) VALUES (3, 'Niedersachsen');
+INSERT INTO postgis_uland(id, uland) VALUES (4, 'Bremen');
+INSERT INTO postgis_uland(id, uland) VALUES (5, 'Nordrhein-Westfalen');
+INSERT INTO postgis_uland(id, uland) VALUES (6, 'Hessen');
+INSERT INTO postgis_uland(id, uland) VALUES (7, 'Rheinland-Pfalz');
+INSERT INTO postgis_uland(id, uland) VALUES (8, 'Baden-Württemberg');
+INSERT INTO postgis_uland(id, uland) VALUES (9, 'Bayern');
+INSERT INTO postgis_uland(id, uland) VALUES (10, 'Saarland');
+INSERT INTO postgis_uland(id, uland) VALUES (11, 'Berlin');
+INSERT INTO postgis_uland(id, uland) VALUES (12, 'Brandenburg');
+INSERT INTO postgis_uland(id, uland) VALUES (13, 'Mecklenburg-Vorpommern');
+INSERT INTO postgis_uland(id, uland) VALUES (14, 'Sachsen');
+INSERT INTO postgis_uland(id, uland) VALUES (15, 'Sachsen-Anhalt');
+INSERT INTO postgis_uland(id, uland) VALUES (16, 'Thüringen');
 
 -- UMONAT Unfallmonat
-INSERT INTO umonat(umonat, name) VALUES (01, 'Januar');
-INSERT INTO umonat(umonat, name) VALUES (02, 'Februar');
-INSERT INTO umonat(umonat, name) VALUES (03, 'März');
-INSERT INTO umonat(umonat, name) VALUES (04, 'April');
-INSERT INTO umonat(umonat, name) VALUES (05, 'Mai');
-INSERT INTO umonat(umonat, name) VALUES (06, 'Juni');
-INSERT INTO umonat(umonat, name) VALUES (07, 'Juli');
-INSERT INTO umonat(umonat, name) VALUES (08, 'August');
-INSERT INTO umonat(umonat, name) VALUES (09, 'September');
-INSERT INTO umonat(umonat, name) VALUES (10, 'Oktober');
-INSERT INTO umonat(umonat, name) VALUES (11, 'November');
-INSERT INTO umonat(umonat, name) VALUES (12, 'Dezember');
+INSERT INTO postgis_umonat(id, umonat) VALUES (1, 'Januar');
+INSERT INTO postgis_umonat(id, umonat) VALUES (2, 'Februar');
+INSERT INTO postgis_umonat(id, umonat) VALUES (3, 'März');
+INSERT INTO postgis_umonat(id, umonat) VALUES (4, 'April');
+INSERT INTO postgis_umonat(id, umonat) VALUES (5, 'Mai');
+INSERT INTO postgis_umonat(id, umonat) VALUES (6, 'Juni');
+INSERT INTO postgis_umonat(id, umonat) VALUES (7, 'Juli');
+INSERT INTO postgis_umonat(id, umonat) VALUES (8, 'August');
+INSERT INTO postgis_umonat(id, umonat) VALUES (9, 'September');
+INSERT INTO postgis_umonat(id, umonat) VALUES (10, 'Oktober');
+INSERT INTO postgis_umonat(id, umonat) VALUES (11, 'November');
+INSERT INTO postgis_umonat(id, umonat) VALUES (12, 'Dezember');
 
 -- UWOCHENTAG Wochentag
-INSERT INTO uwochentag(uwochentag, name) VALUES (1, 'Sonntag');
-INSERT INTO uwochentag(uwochentag, name) VALUES (2, 'Montag');
-INSERT INTO uwochentag(uwochentag, name) VALUES (3, 'Dienstag');
-INSERT INTO uwochentag(uwochentag, name) VALUES (4, 'Mittwoch');
-INSERT INTO uwochentag(uwochentag, name) VALUES (5, 'Donnerstag');
-INSERT INTO uwochentag(uwochentag, name) VALUES (6, 'Freitag');
-INSERT INTO uwochentag(uwochentag, name) VALUES (7, 'Samstag');
+INSERT INTO postgis_uwochentag(id, uwochentag) VALUES (1, 'Sonntag');
+INSERT INTO postgis_uwochentag(id, uwochentag) VALUES (2, 'Montag');
+INSERT INTO postgis_uwochentag(id, uwochentag) VALUES (3, 'Dienstag');
+INSERT INTO postgis_uwochentag(id, uwochentag) VALUES (4, 'Mittwoch');
+INSERT INTO postgis_uwochentag(id, uwochentag) VALUES (5, 'Donnerstag');
+INSERT INTO postgis_uwochentag(id, uwochentag) VALUES (6, 'Freitag');
+INSERT INTO postgis_uwochentag(id, uwochentag) VALUES (7, 'Samstag');
 
 -- UKATEGORIE Unfallkategorie
-INSERT INTO ukategorie(ukategorie, name) VALUES (1, 'Unfall mit Getöteten');
-INSERT INTO ukategorie(ukategorie, name) VALUES (2, 'Unfall mit Schwerverletzten');
-INSERT INTO ukategorie(ukategorie, name) VALUES (3, 'Unfall mit Leichtverletzten');
+INSERT INTO postgis_ukategorie(id, ukategorie) VALUES (1, 'Unfall mit Getöteten');
+INSERT INTO postgis_ukategorie(id, ukategorie) VALUES (2, 'Unfall mit Schwerverletzten');
+INSERT INTO postgis_ukategorie(id, ukategorie) VALUES (3, 'Unfall mit Leichtverletzten');
 
 -- UART Unfallart
-INSERT INTO uart(uart, name) VALUES (1, 'Zusammenstoß mit anfahrendem/anhaltendem/ruhendem Fahrzeug');
-INSERT INTO uart(uart, name) VALUES (2, 'Zusammenstoß mit vorausfahrendem / wartendem Fahrzeug');
-INSERT INTO uart(uart, name) VALUES (3, 'Zusammenstoß mit seitlich in gleicher Richtung fahrendem Fahrzeug');
-INSERT INTO uart(uart, name) VALUES (4, 'Zusammenstoß mit entgegenkommendem Fahrzeug');
-INSERT INTO uart(uart, name) VALUES (5, 'Zusammenstoß mit einbiegendem / kreuzendem Fahrzeug');
-INSERT INTO uart(uart, name) VALUES (6, 'Zusammenstoß zwischen Fahrzeug und Fußgänger');
-INSERT INTO uart(uart, name) VALUES (7, 'Aufprall auf Fahrbahnhindernis');
-INSERT INTO uart(uart, name) VALUES (8, 'Abkommen von Fahrbahn nach rechts');
-INSERT INTO uart(uart, name) VALUES (9, 'Abkommen von Fahrbahn nach links');
-INSERT INTO uart(uart, name) VALUES (0, 'Unfall anderer Art');
+INSERT INTO postgis_uart(id, uart) VALUES (1, 'Zusammenstoß mit anfahrendem/anhaltendem/ruhendem Fahrzeug');
+INSERT INTO postgis_uart(id, uart) VALUES (2, 'Zusammenstoß mit vorausfahrendem / wartendem Fahrzeug');
+INSERT INTO postgis_uart(id, uart) VALUES (3, 'Zusammenstoß mit seitlich in gleicher Richtung fahrendem Fahrzeug');
+INSERT INTO postgis_uart(id, uart) VALUES (4, 'Zusammenstoß mit entgegenkommendem Fahrzeug');
+INSERT INTO postgis_uart(id, uart) VALUES (5, 'Zusammenstoß mit einbiegendem / kreuzendem Fahrzeug');
+INSERT INTO postgis_uart(id, uart) VALUES (6, 'Zusammenstoß zwischen Fahrzeug und Fußgänger');
+INSERT INTO postgis_uart(id, uart) VALUES (7, 'Aufprall auf Fahrbahnhindernis');
+INSERT INTO postgis_uart(id, uart) VALUES (8, 'Abkommen von Fahrbahn nach rechts');
+INSERT INTO postgis_uart(id, uart) VALUES (9, 'Abkommen von Fahrbahn nach links');
+INSERT INTO postgis_uart(id, uart) VALUES (0, 'Unfall anderer Art');
 
 -- UTYP1 Unfalltyp
-INSERT INTO utyp1(utyp1, name) VALUES (1, 'Fahrunfall');
-INSERT INTO utyp1(utyp1, name) VALUES (2, 'Abbiegeunfall');
-INSERT INTO utyp1(utyp1, name) VALUES (3, 'Einbiegen / Kreuzen-Unfall');
-INSERT INTO utyp1(utyp1, name) VALUES (4, 'Überschreiten-Unfall');
-INSERT INTO utyp1(utyp1, name) VALUES (5, 'Unfall durch ruhenden Verkehr');
-INSERT INTO utyp1(utyp1, name) VALUES (6, 'Unfall im Längsverkehr');
-INSERT INTO utyp1(utyp1, name) VALUES (7, 'sonstiger Unfall');
+INSERT INTO postgis_utyp1(id, utyp1) VALUES (1, 'Fahrunfall');
+INSERT INTO postgis_utyp1(id, utyp1) VALUES (2, 'Abbiegeunfall');
+INSERT INTO postgis_utyp1(id, utyp1) VALUES (3, 'Einbiegen / Kreuzen-Unfall');
+INSERT INTO postgis_utyp1(id, utyp1) VALUES (4, 'Überschreiten-Unfall');
+INSERT INTO postgis_utyp1(id, utyp1) VALUES (5, 'Unfall durch ruhenden Verkehr');
+INSERT INTO postgis_utyp1(id, utyp1) VALUES (6, 'Unfall im Längsverkehr');
+INSERT INTO postgis_utyp1(id, utyp1) VALUES (7, 'sonstiger Unfall');
 
 -- ULICHTVERH Lichtverhältnisse:
-INSERT INTO ulichtverh(ulichtverh, name) VALUES (0, 'Tageslicht');
-INSERT INTO ulichtverh(ulichtverh, name) VALUES (1, 'Dämmerung');
-INSERT INTO ulichtverh(ulichtverh, name) VALUES (2, 'Dunkelheit');
+INSERT INTO postgis_ulichtverh(id, ulichtverh) VALUES (0, 'Tageslicht');
+INSERT INTO postgis_ulichtverh(id, ulichtverh) VALUES (1, 'Dämmerung');
+INSERT INTO postgis_ulichtverh(id, ulichtverh) VALUES (2, 'Dunkelheit');
 
 -- IstRad Unfall mit Rad
-INSERT INTO istrad(istrad, name) VALUES (0, 'Unfall ohne Fahrradbeteiligung');
-INSERT INTO istrad(istrad, name) VALUES (1, 'Unfall mit Fahrradbeteiligung');
+INSERT INTO postgis_istrad(id, istrad) VALUES (0, 'Unfall ohne Fahrradbeteiligung');
+INSERT INTO postgis_istrad(id, istrad) VALUES (1, 'Unfall mit Fahrradbeteiligung');
 
 -- IstPKW Unfall mit Pkw
-INSERT INTO istpkw(istpkw, name) VALUES (0, 'Unfall ohne PKW-Beteiligung');
-INSERT INTO istpkw(istpkw, name) VALUES (1, 'Unfall mit PKW-Beteiligung');
+INSERT INTO postgis_istpkw(id, istpkw) VALUES (0, 'Unfall ohne PKW-Beteiligung');
+INSERT INTO postgis_istpkw(id, istpkw) VALUES (1, 'Unfall mit PKW-Beteiligung');
 
 -- IstFuss Unfall mit Fußgänger
-INSERT INTO istfuss(istfuss, name) VALUES (0, 'Unfall ohne Fußgängerbeteiligung');
-INSERT INTO istfuss(istfuss, name) VALUES (1, 'Unfall mit Fußgängerbeteiligung');
+INSERT INTO postgis_istfuss(id, istfuss) VALUES (0, 'Unfall ohne Fußgängerbeteiligung');
+INSERT INTO postgis_istfuss(id, istfuss) VALUES (1, 'Unfall mit Fußgängerbeteiligung');
 
 -- IstKrad Unfall mit Kraftrad
-INSERT INTO istkrad(istkrad, name) VALUES (0, 'Unfall ohne Kraftradbeteiligung');
-INSERT INTO istkrad(istkrad, name) VALUES (1, 'Unfall mit Kraftradbeteiligung');
+INSERT INTO postgis_istkrad(id, istkrad) VALUES (0, 'Unfall ohne Kraftradbeteiligung');
+INSERT INTO postgis_istkrad(id, istkrad) VALUES (1, 'Unfall mit Kraftradbeteiligung');
 
 -- IstGkfz Unfall mit Güterkraftfahrzeug (GKFZ)
-INSERT INTO istgkfz(istgkfz, name) VALUES (0, 'Unfall ohne Güterkraftfahrzeugbeteiligung');
-INSERT INTO istgkfz(istgkfz, name) VALUES (1, 'Unfall mit Güterkraftfahrzeugbeteiligung');
+INSERT INTO postgis_istgkfz(id, istgkfz) VALUES (0, 'Unfall ohne Güterkraftfahrzeugbeteiligung');
+INSERT INTO postgis_istgkfz(id, istgkfz) VALUES (1, 'Unfall mit Güterkraftfahrzeugbeteiligung');
 
 -- IstSonstig Unfall mit Sonstigen
-INSERT INTO istsonstig(istsonstig, name) VALUES (0, 'Unfall ohne Beteiligung eines oben nicht genannten Verkehrsmittels');
-INSERT INTO istsonstig(istsonstig, name) VALUES (1, 'Unfall mit Beteiligung eines oben nicht genannten Verkehrsmittels');
+INSERT INTO postgis_istsonstig(id, istsonstig) VALUES (0, 'Unfall ohne Beteiligung eines oben nicht genannten Verkehrsmittels');
+INSERT INTO postgis_istsonstig(id, istsonstig) VALUES (1, 'Unfall mit Beteiligung eines oben nicht genannten Verkehrsmittels');
 
 -- USTRZUSTAND Straßenzustand
-INSERT INTO ustrzustan(ustrzustan, name) VALUES (0, 'trocken');
-INSERT INTO ustrzustan(ustrzustan, name) VALUES (1, 'nass/feucht/schlüpfrig');
-INSERT INTO ustrzustan(ustrzustan, name) VALUES (2, 'winterglatt');
+INSERT INTO postgis_ustrzustan(id, ustrzustan) VALUES (0, 'trocken');
+INSERT INTO postgis_ustrzustan(id, ustrzustan) VALUES (1, 'nass/feucht/schlüpfrig');
+INSERT INTO postgis_ustrzustan(id, ustrzustan) VALUES (2, 'winterglatt');
